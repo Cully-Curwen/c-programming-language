05f8184402ddda6cea4ee899c98a178271239843
Cully-Curwen <c.t.g.curwen@gmail.com>
2026-05-15 16:47:38 +0100

    ex 7-3

---
+++ chapter7/minprintf.c
@@ -0,0 +1,83 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/* minprintf: minimal printf with variable argument list */
+void minprintf(char *fmt, ...) {
+        va_list ap;    /* points to each unnamed arg in turn */
+        char *p, *sval;
+        int ival;
+        double dval;
+        unsigned uval;
+        void *pval;
+
+        va_start(ap, fmt);      /* make ap point to 1st unnamed arg */
+        for (p = fmt; *p; p++) {
+                if (*p != '%') {
+                        putchar(*p);
+                        continue;
+                }
+                switch (*++p) {
+                case 'd':
+                case 'i':
+                        ival = va_arg(ap, int);
+                        printf("%d", ival);
+                        break;
+                case 'c':
+                        ival = va_arg(ap, int);
+                        printf("%c", ival);
+                        break;
+                case 'o':
+                        uval = va_arg(ap, unsigned);
+                        printf("%o", uval);
+                        break;
+                case 'x':
+                        uval = va_arg(ap, unsigned);
+                        printf("%x", uval);
+                        break;
+                case 'X':
+                        uval = va_arg(ap, unsigned);
+                        printf("%X", uval);
+                        break;
+                case 'u':
+                        uval = va_arg(ap, unsigned);
+                        printf("%u", uval);
+                        break;
+                case 'f':
+                        dval = va_arg(ap, double);
+                        printf("%f", dval);
+                        break;
+                case 'e':
+                        dval = va_arg(ap, double);
+                        printf("%e", dval);
+                        break;
+                case 'E':
+                        dval = va_arg(ap, double);
+                        printf("%E", dval);
+                        break;
+                case 'g':
+                        dval = va_arg(ap, double);
+                        printf("%g", dval);
+                        break;
+                case 'G':
+                        dval = va_arg(ap, double);
+                        printf("%G", dval);
+                        break;
+                case 's':
+                        for (sval = va_arg(ap, char *); *sval; sval++)
+                                putchar(*sval);
+                        break;
+                case 'p':
+                        pval = va_arg(ap, void *);
+                        printf("%p", pval);
+                        break;
+                case '%':
+                        printf("%%");
+                        break;
+                default:
+                        putchar(*p);
+                        break;
+                }
+        }
+        va_end(ap);     /* clean up when done */
+}
+
